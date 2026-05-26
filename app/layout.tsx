86dd935d022b3d85b3d5933dd679c494c5b7ffe1
Dick Marjanovic <dick.marjanovic@accenture.com>
2026-05-26 11:55:55 +0100

    move firebase api details to .env

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,15 +4,17 @@ import Footer from '../src/components/Footer'
 import '../styles/globals.scss'
 import { Metadata } from 'next'
 
-initCloudSync({
-  apiKey: "AIzaSyBJNN0MquAuQ1XcDqYqnZ3boJKwk0VwXqQ",
-  authDomain: "groovly-2a756.firebaseapp.com",
-  projectId: "groovly-2a756",
-  storageBucket: "groovly-2a756.firebasestorage.app",
-  messagingSenderId: "75107342085",
-  appId: "1:75107342085:web:d182977d6dcafdf61b096c",
-  measurementId: "G-H5ZMDXHGQN"
-});
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+}
+
+initCloudSync(firebaseConfig)
 
 export const metadata: Metadata = {
   title: 'Music Tools PWA',
